4a6ce6bdb2b797545890d335d212385e8f7c0cd9
rorycl <rorycl@campbell-lange.net>
2025-07-17 08:38:13 +0100

    marklogic: add setting stemmed-searches to true in deploy.sh
    This also adds the admin port and target database to the set of
    environmental variables used.

--- src/marklogic/deploy.sh
+++ src/marklogic/deploy.sh
@@ -21,6 +21,37 @@ if [ -z "${ML_PORT}" ]; then
 	echo "ML_PORT (normally 8000) not defined, quitting"
 	exit 1
 fi
+if [ -z "${ML_ADMIN_PORT}" ]; then
+	echo "ML_ADMIN_PORT (normally 8002) not defined, quitting"
+	exit 1
+fi
+if [ -z "${ML_ADMIN_DATABASE}" ]; then
+	echo "ML_ADMIN_DATABASE (normally Documents) not defined, quitting"
+	exit 1
+fi
+
+SHOW_DATABASES=0
+
+if [ $SHOW_DATABASES -gt 0 ]; then
+    # admin: show databases
+    curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -X GET \
+      "http://${ML_HOST}:${ML_ADMIN_PORT}/manage/v2/databases/"
+fi
+
+# Ensure search stemming is on at the administrative level;
+#   choices are basic, advanced or off.
+# General documentation:
+#   https://docs.marklogic.com/REST/management/databases
+# Specific documentation:
+#   https://docs.marklogic.com/REST/PUT/manage/v2/databases/[id-or-name]/properties
+#   /manage/v2/databases/{id|name}/properties
+echo "Setting stemmed searches to basic: http://${ML_HOST}:${ML_ADMIN_PORT}/manage/v2/databases/${ML_ADMIN_DATABASE}/properties"
+curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -X PUT \
+  --header "Content-Type:application/xml" \
+  -d '<database-properties xmlns="http://marklogic.com/manage"><stemmed-searches>basic</stemmed-searches></database-properties>' \
+  "http://${ML_HOST}:${ML_ADMIN_PORT}/manage/v2/databases/${ML_ADMIN_DATABASE}/properties"
+
+exit 0
 
 # deploy summaries
 FILE=summaries.xqy
@@ -30,7 +61,7 @@ curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -X PUT -i \
 	-H "Content-type: application/xquery" \
 	--data-binary @${FILE} \
 	"http://${ML_HOST}:${ML_PORT}/v1/ext/${ENDPOINT}"
-	
+
 # should return:
 #
 # HTTP/1.1 204 Updated
@@ -45,8 +76,8 @@ curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -i -X POST \
 	 --data-urlencode vars='{"sort_by": "date", "sort_direction": "desc"}' \
      http://${ML_HOST}:${ML_PORT}/LATEST/invoke
 
-# should return something like: 
-# 
+# should return something like:
+#
 # HTTP/1.1 200 OK
 # Server: MarkLogic 11.3.1
 # Set-Cookie: TxnID=null; path=/; SameSite=Lax
@@ -54,15 +85,12 @@ curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -i -X POST \
 # Content-Length: 840
 # Connection: Keep-Alive
 # Keep-Alive: timeout=5
-# 
-# 
+#
+#
 # --bb095a5d570af3af
 # Content-Type: application/xml
 # X-Primitive: element()
 # X-Path: /summaries
-# 
+#
 # <summaries><summary><uri>/documents/ewhc_ch_2025_16.xml</uri><name>SBP 2 S.À.R.L v 2 SOUTHBANK TENANT LIMITED</name><judgmentDate>2025-01-07</judgmentDate><court>EWHC-Chancery</court><citation>[2025] EWHC 16 (Ch)</citation></summary><summary><uri>/documents/ewhc_ch_2008_1582.xml</uri><name>Landlord Protect Ltd. v St Anselm Development Company Ltd.</name><judgmentDate>2008-07-08</judgmentDate><court>EWHC-Chancery</court><citation>[2008] EWHC 1582 (Ch)</citation></summary><summary><uri>/documents/ewhc_ch_2004_324.xml</uri><name>Design Progression Ltd v Thurloe Properties Ltd</name><judgmentDate>2004-02-25</judgmentDate><court>EWHC-Chancery</court><citation>[2004] EWHC 324 (Ch)</citation></summary></summaries>
 # --bb095a5d570af3af--
-
-
-
