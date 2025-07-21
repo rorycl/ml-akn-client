c006027b1eea0c1a0db8fc1804fbad19df591762
rorycl <rorycl@campbell-lange.net>
2025-07-21 09:37:27 +0100

    marklogic: update deployment to add library, fail-fast improvements

--- src/marklogic/deploy.sh
+++ src/marklogic/deploy.sh
@@ -31,6 +31,7 @@ if [ -z "${ML_ADMIN_DATABASE}" ]; then
 fi
 
 SHOW_DATABASES=0
+SET_STEMMING=1
 
 if [ $SHOW_DATABASES -gt 0 ]; then
     # admin: show databases
@@ -41,18 +42,38 @@ fi
 # ----------------------------------------------------------------------
 # admin
 
-# Ensure search stemming is on at the administrative level;
-#   choices are basic, advanced or off.
-# General documentation:
-#   https://docs.marklogic.com/REST/management/databases
-# Specific documentation:
-#   https://docs.marklogic.com/REST/PUT/manage/v2/databases/[id-or-name]/properties
-#   /manage/v2/databases/{id|name}/properties
-echo "Setting stemmed searches to basic: http://${ML_HOST}:${ML_ADMIN_PORT}/manage/v2/databases/${ML_ADMIN_DATABASE}/properties"
-curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -X PUT \
-  --header "Content-Type:application/xml" \
-  -d '<database-properties xmlns="http://marklogic.com/manage"><stemmed-searches>basic</stemmed-searches></database-properties>' \
-  "http://${ML_HOST}:${ML_ADMIN_PORT}/manage/v2/databases/${ML_ADMIN_DATABASE}/properties"
+if [ $SET_STEMMING -gt 0 ]; then
+    # Ensure search stemming is on at the administrative level;
+    #   choices are basic, advanced or off.
+    # General documentation:
+    #   https://docs.marklogic.com/REST/management/databases
+    # Specific documentation:
+    #   https://docs.marklogic.com/REST/PUT/manage/v2/databases/[id-or-name]/properties
+    #   /manage/v2/databases/{id|name}/properties
+    echo "Setting stemmed searches to basic: http://${ML_HOST}:${ML_ADMIN_PORT}/manage/v2/databases/${ML_ADMIN_DATABASE}/properties"
+    curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -X PUT \
+      --header "Content-Type:application/xml" \
+      -d '<database-properties xmlns="http://marklogic.com/manage"><stemmed-searches>basic</stemmed-searches></database-properties>' \
+      --fail-with-body \
+      "http://${ML_HOST}:${ML_ADMIN_PORT}/manage/v2/databases/${ML_ADMIN_DATABASE}/properties"
+fi
+
+# ----------------------------------------------------------------------
+# summaries-lib (a general-purpose summaries library module)
+
+# deploy summaries-lib
+FILE=summaries-lib.xqy
+ENDPOINT=summaries-lib.xqy
+
+echo "---------------------------------------------------------"
+echo "deploying $FILE to $ENDPOINT"
+echo "---------------------------------------------------------"
+
+curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -X PUT -i \
+	-H "Content-type: application/xquery" \
+	--data-binary @${FILE} \
+    --fail-with-body \
+	"http://${ML_HOST}:${ML_PORT}/v1/ext/${ENDPOINT}"
 
 # ----------------------------------------------------------------------
 # summaries
@@ -68,18 +89,21 @@ echo "---------------------------------------------------------"
 curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -X PUT -i \
 	-H "Content-type: application/xquery" \
 	--data-binary @${FILE} \
+    --fail-with-body \
 	"http://${ML_HOST}:${ML_PORT}/v1/ext/${ENDPOINT}"
+
 
 echo "---------------------------------------------------------"
 echo "querying $ENDPOINT"
 echo "---------------------------------------------------------"
 
 curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -i -X POST \
-     -H "Content-type: application/x-www-form-urlencoded" \
-     --data-urlencode module=/ext/${ENDPOINT} \
-	 --data-urlencode vars='{"sort_by": "date", "sort_direction": "desc"}' \
-     http://${ML_HOST}:${ML_PORT}/LATEST/invoke
-     #
+    -H "Content-type: application/x-www-form-urlencoded" \
+    --data-urlencode module=/ext/${ENDPOINT} \
+	--data-urlencode vars='{"sort_by": "date", "sort_direction": "desc"}' \
+    --fail-with-body \
+    http://${ML_HOST}:${ML_PORT}/LATEST/invoke
+
 # ----------------------------------------------------------------------
 # search
 
@@ -94,6 +118,7 @@ echo "---------------------------------------------------------"
 curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -X PUT -i \
 	-H "Content-type: application/xquery" \
 	--data-binary @${FILE} \
+    --fail-with-body \
 	"http://${ML_HOST}:${ML_PORT}/v1/ext/${ENDPOINT}"
 
 echo "---------------------------------------------------------"
@@ -101,7 +126,8 @@ echo "querying $ENDPOINT"
 echo "---------------------------------------------------------"
 
 curl --digest --user ${ML_USERNAME}:${ML_PASSWORD} -i -X POST \
-     -H "Content-type: application/x-www-form-urlencoded" \
-     --data-urlencode module=/ext/${ENDPOINT} \
-	 --data-urlencode vars='{"query": "norwich", "sort_by": "date", "sort_direction": "desc"}' \
-     http://${ML_HOST}:${ML_PORT}/LATEST/invoke
+    -H "Content-type: application/x-www-form-urlencoded" \
+    --data-urlencode module=/ext/${ENDPOINT} \
+	--data-urlencode vars='{"query": "norwich", "sort_by": "date", "sort_direction": "desc"}' \
+    --fail-with-body \
+    http://${ML_HOST}:${ML_PORT}/LATEST/invoke
